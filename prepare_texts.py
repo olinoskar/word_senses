18124ec3c81b7f68f1745f44d3803449ed9d9a6e
olinoskar <olinoskar@gmail.com>
2020-06-10 12:17:36 +0200

    Updated prepare_texts and added an analysis directory

--- prepare_texts.py
+++ prepare_texts.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+import spacy
+from tqdm import tqdm
 
 """
 Got to the link below for books in multiple languages: 
@@ -9,7 +12,7 @@ http://farkastranslations.com/bilingual_books.php
 
 URLS = [
     'http://farkastranslations.com/books/Verne_Jules-Ile_mysterieuse-fr-en-es-hu.html',
-    #'http://farkastranslations.com/books/Doyle_Arthur_Conan-A_Study_in_Scarlet-en-fr-es.html'
+    'http://farkastranslations.com/books/Dumas_Alexandre-Trois_Mousquetaires-fr-en-hu-es-nl.html',
     ]
 LANGUAGES = [
     'English', 'French', 'Spanish'
@@ -18,12 +21,26 @@ LANGUAGES = [
 FNAME_WORD_ALIGNMENT = 'data/word_alignment.txt'
 FNAME_BERT = 'data/bert.txt'
 
+try:
+    with open('data/lemmas.txt', 'r') as f:
+        WORDS = [word.strip() for word in f.readlines()]
+except FileNotFoundError:
+    WORDS = None
+
+print(WORDS)
+
+
 
 def main():
 
     df = get_data_from_urls(URLS)
+
     prepare_for_word_alignment(df, fname = FNAME_WORD_ALIGNMENT)
     prepare_for_bert(df, fname = FNAME_BERT)
+
+    print_stats(df)
+
+    
 
 
 
@@ -40,9 +57,6 @@ def get_data_from_urls(urls):
 
 def get_data_from_url(url):
     resp = requests.get(url)
-
-
-
     df = pd.read_html(resp.content)[0]
     df.columns = df.loc[0].values
     df = df.drop(0).reset_index(drop=True)
@@ -51,7 +65,7 @@ def get_data_from_url(url):
 
 
 ###############################
-# Prepare and write data
+# Prepare and write data to files
 
 def prepare_for_word_alignment(df, fname = None):
     if fname:
@@ -73,9 +87,6 @@ def prepare_for_bert(df, fname = None):
 
 
 
-        
-
-
 
 
 ###############################
@@ -83,20 +94,51 @@ def prepare_for_bert(df, fname = None):
 
 
 def print_stats(df, language = 'English'):
+
+    print("Loading Spacy...")
+    sp = spacy.load('en_core_web_md')
+    print("Spacy loaded\n")
+
+    print('\nSTATS FOR', language.upper())
+    print('-'* (10 + len(language)))
     print('Number of texts:', len(df))
-    print(df)
+
 
     series = df[language].dropna()
 
-    unique_words = set()
+    lemmas = []
+
     num = 0
-    for text in series:
+    for text in tqdm(series, desc = 'Counting lemmas and words'):
+        for token in sp(text):
+            #if token.lemma_ not in ['-PRON-', ',', '.', '.', '"', '!', '?', ';', '-']
+            if token.pos_ in ["PUNCT", "PRON"]:
+                continue
+            lemmas.append(token.lemma_)
         num += len(text.split(' '))
-        for word in text.split(' '):
-            unique_words.add(word)
+
 
     print('Number of words:', num)
-    print('Number of unique words:', len(unique_words))
+    print('Number of lemmas:', len(set(lemmas)))
+
+    print()
+
+    occurences = {}
+    for word in lemmas:
+        try:
+            occurences[word] += 1
+        except KeyError:
+            occurences[word] = 1
+
+    df = pd.DataFrame(occurences.items(), columns = ['Lemma', 'Occurences'])
+    df = df.sort_values(by = 'Occurences', ascending = False)
+    df = df.reset_index(drop = True)
+
+    if WORDS:
+        print(df[df['Lemma'].isin(WORDS)])
+    else:
+        print(df.loc[:50])
+    
 
 
 
